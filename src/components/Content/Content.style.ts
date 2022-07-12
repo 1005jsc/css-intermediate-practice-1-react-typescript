027973efa9c31aba20c8577e4d33cd12cb2af3f8
cho_jae_sin <1005jsc@naver.com>
2022-07-12 21:32:51 +0900

    content animation added

--- src/components/Content/Content.style.ts
+++ src/components/Content/Content.style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Button, Section } from '../../globalStyle.style';
+import { motion } from 'framer-motion';
 
 export const ContentSection = styled(Section)`
   border: 1px solid white;
@@ -9,7 +10,7 @@ type ContentDivProps = {
   reverse: boolean;
 };
 
-export const ContentDiv = styled.div<ContentDivProps>`
+export const ContentDiv = styled(motion.div)<ContentDivProps>`
   display: flex;
   justify-content: center;
   flex-flow: ${({ reverse }) => (reverse ? 'row-reverse' : 'row')};
@@ -17,11 +18,11 @@ export const ContentDiv = styled.div<ContentDivProps>`
   height: 30rem;
 `;
 
-export const ContentImageDiv = styled.div`
+export const ContentImageDiv = styled(motion.div)`
   height: 24rem;
 `;
 
-export const ContentImageImg = styled.img`
+export const ContentImageImg = styled(motion.img)`
   height: 100%;
 `;
 
@@ -31,18 +32,18 @@ export const ContentMetadataDiv = styled.div`
   padding: 0 1rem;
 `;
 
-export const ContentMetadataP1 = styled.p`
+export const ContentMetadataP1 = styled(motion.p)`
   font-size: 0.8rem;
   color: gray;
   letter-spacing: 0.1rem;
 `;
-export const ContentMetadataP2 = styled.p`
+export const ContentMetadataP2 = styled(motion.p)`
   font-size: 2.6rem;
   font-weight: 800;
   margin-top: 1rem;
   margin-bottom: 1rem;
 `;
-export const ContentMetadataP3 = styled.p`
+export const ContentMetadataP3 = styled(motion.p)`
   font-size: 1.1rem;
   letter-spacing: 0.06rem;
   color: gray;
@@ -50,7 +51,16 @@ export const ContentMetadataP3 = styled.p`
   margin-bottom: 2rem;
 `;
 
-export const ContentButton = styled(Button)`
+export const ContentButton = styled(motion.button)`
+  width: 8rem;
+  height: 2.6rem;
+  background-color: transparent;
+  border: 2px solid white;
+  border-radius: 0.5rem;
+  color: white;
+  font-size: 1.1rem;
+  letter-spacing: 0.08rem;
+  cursor: pointer;
   color: blue;
   border: 2px solid blue;
   border-radius: 0.4rem;
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,3 +1,4 @@
+import { useAnimation } from 'framer-motion';
 import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { Button, Container } from '../../globalStyle.style';
@@ -21,31 +22,75 @@ type ContentProps = {
 
 const Content = ({ headline, description, buttonLabel }: ContentProps) => {
   const { ref, inView, entry } = useInView({
-    threshold: 0,
+    threshold: 0.3,
   });
-
+  const animation = useAnimation();
+  const initial = {
+    opacity: 0,
+    y: 20,
+  };
   useEffect(() => {
     console.log(inView);
     console.log(entry);
+
+    if (inView) {
+      animation.start({
+        opacity: 1,
+        y: 0,
+      });
+    } else {
+      animation.start({
+        opacity: 0,
+        y: 30,
+      });
+    }
   }, [entry]);
 
   return (
     <ContentSection>
       <Container>
         <ContentDiv ref={ref} reverse={false}>
-          <ContentImageDiv>
-            <ContentImageImg src='./assets/svg/Deal.svg' />
+          <ContentImageDiv
+            initial={initial}
+            animate={animation}
+            transition={inView ? { delay: 0.3, duration: 0.6 } : {}}
+          >
+            <ContentImageImg
+              src='./assets/svg/Deal.svg'
+              whileHover={{ scale: 1.02, rotate: 2 }}
+              transition={{ duration: 0.5 }}
+            />
           </ContentImageDiv>
           <ContentMetadataDiv>
-            <ContentMetadataP1>Founded in 2014</ContentMetadataP1>
-            <ContentMetadataP2>
+            <ContentMetadataP1
+              initial={initial}
+              animate={animation}
+              transition={inView ? { delay: 0.4, duration: 0.6 } : {}}
+            >
+              Founded in 2014
+            </ContentMetadataP1>
+            <ContentMetadataP2
+              initial={initial}
+              animate={animation}
+              transition={inView ? { delay: 0.6, duration: 0.6 } : {}}
+            >
               We've been in <br />
               business for 9 years
             </ContentMetadataP2>
-            <ContentMetadataP3>
+            <ContentMetadataP3
+              initial={initial}
+              animate={animation}
+              transition={inView ? { delay: 0.8, duration: 0.6 } : {}}
+            >
               We have designed and implemented the best cloud security solutions
             </ContentMetadataP3>
-            <ContentButton>Find More</ContentButton>
+            <ContentButton
+              initial={initial}
+              animate={animation}
+              transition={inView ? { delay: 1.2, duration: 0.8 } : {}}
+            >
+              Find More
+            </ContentButton>
           </ContentMetadataDiv>
         </ContentDiv>
       </Container>
