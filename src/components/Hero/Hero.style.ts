f4eef724f295cf7430656d6f33acf7c5d01b25d2
cho_jae_sin <1005jsc@naver.com>
2022-07-15 07:30:25 +0900

    hero width fixed

--- src/components/Hero/Hero.style.ts
+++ src/components/Hero/Hero.style.ts
@@ -4,6 +4,7 @@ import { Container, Section } from '../../globalStyle.style';
 export const HeroSection = styled(Section)`
   /* border: 1px solid white; */
   /* height: 100vh; */
+  width: 98vw;
   height: calc(100vh - 15px);
   padding-top: clamp(70px, 25vh, 220px);
 `;
@@ -12,6 +13,7 @@ export const HeroContainer = styled(Container)``;
 
 export const HeroVideo = styled.video`
   object-fit: cover;
+  width: 100%;
   height: 100%;
   /* background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)); */
   position: absolute;
